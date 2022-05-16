e686eb14795dedb96274e26765db3212c1b78fd6
ricky-blip <ricky.rinaldy77@gmail.com>
2022-05-16 12:44:18 +0700

    update

---
+++ constant.dart
@@ -0,0 +1,15 @@
+void main() {
+  //keyword final 
+  final name = 'r';
+  final String name2 = 'a';
+
+  print(name);
+  print(name2);
+
+  //keyword const
+  final nomor = 77;
+  final int number = 007;
+
+  print(nomor);
+  print(number);
+}
