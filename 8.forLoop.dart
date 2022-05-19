0c0409142b01c5b79b8b03debe2d57d30ce7b10f
ricky-blip <ricky.rinaldy77@gmail.com>
2022-05-19 13:37:03 +0700

    update

--- 8.forLoop.dart
+++ 8.forLoop.dart
@@ -1,3 +1,21 @@
 void main() {
-  
+  /** NOTE
+   * Contoh kode loopint untuk menampilkan angka 1 sampai 5
+   * 
+   * for (int i = 1; i <= 10; i++) {
+   *  print(i);
+   * }
+   */
+
+  // NOTE Perulangan FOR
+  for (int i = 1; i <= 10; i++) {
+    print('For "Looping" : $i');
+  }
+
+  // NOTE Perulangan FOR in
+  List angka = ['For in "Looping"',7, 8, 9,];
+
+  for (var item in angka) {
+    print(item);
+  }
 }
