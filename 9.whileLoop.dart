1c9e9cfab262f651b94986fc45b897cf7324998f
ricky-blip <ricky.rinaldy77@gmail.com>
2022-05-20 07:25:22 +0700

    update

---
+++ 9.whileLoop.dart
@@ -0,0 +1,19 @@
+void main() {
+  /** NOTE
+   * Contoh kode loopint untuk menampilkan angka 1 sampai 5
+   * 
+   * int i = 1;
+   * while (i <=10) {
+   *    print(i);
+   *    i++; sama saja seperti i = i + 1
+   * }
+   * 
+  */
+
+  int i = 1;
+
+  while (i <= 5) {
+    print('While Looping : $i');
+    i++;
+  }
+}
