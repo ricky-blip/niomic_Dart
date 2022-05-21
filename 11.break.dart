56190cfe7c6037de95a3fd564a01e6181c595d1f
ricky-blip <ricky.rinaldy77@gmail.com>
2022-05-21 20:06:15 +0700

    update

--- 11.break.dart
+++ 11.break.dart
@@ -1,3 +1,13 @@
 void main() {
+  outerLoop: for (int i = 1; i <= 3; i++) { 
+    innerLoop: for (int j = 1; j <=3; j++) { 
+      print("Out $i | In $j"); 
+
+      if (i == 2 && j == 2) {
+        break outerLoop;
+      }
+    }
+  }
   
+  print("Break Looping Luar ke-2 dan dalam ke-2");
 }
