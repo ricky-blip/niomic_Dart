c2d124cdc18a2c7cc229ab71d20c57fc4b7c4382
ricky-blip <ricky.rinaldy77@gmail.com>
2022-05-22 19:31:56 +0700

    update

--- 12.continue.dart
+++ 12.continue.dart
@@ -1,3 +1,10 @@
 void main() {
-  
+  Outer: for (int i = 1; i <= 3; i++) {
+    for (int j = 1; j <= 3; j++) {
+      if (i == 1 && j == 3) {
+        continue Outer;
+      }
+      print("$i $j");
+    }
+  }
 }
